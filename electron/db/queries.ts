e564bba351db66c8204064b74ab9a04dce1b1ef4
Kristoffer Josh Tesaluna <145976406+opsnight@users.noreply.github.com>
2025-12-04 11:19:58 +0800

    updated database schema for inventory

--- electron/db/queries.ts
+++ electron/db/queries.ts
@@ -1,13 +1,6 @@
 import { db } from './db';
 
 export function getProducts() {
-  return new Promise((resolve, reject) => {
-    db.all('SELECT * FROM products', (err, rows) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(rows);
-      }
-    });
-  });
+  const stmt = db.prepare('SELECT * FROM products');
+  return stmt.all();
 }
